3cbe83a7cf39be2b1a61598cdab577da2e379099
trinhtran02102006-hash <trinhtran02102006@gmail.com>
2025-12-01 12:43:31 +0700

    Update auth.h

--- lib/headers/auth.h
+++ lib/headers/auth.h
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-// Struct chứa các đường dẫn file của user
+
 struct UserDataPaths {
     string nhanvienPath;
     string sanphamPath;
@@ -25,23 +25,23 @@ private:
     UserDataPaths dataPaths;
 
 public:
-    // Constructor
+    
     User() {}
     User(string uid_, string username_, string email_, string pwd_, UserDataPaths paths_)
         : uid(uid_), username(username_), email(email_), password(pwd_), dataPaths(paths_) {}
 
-    // Static methods for authentication
+    
     static User* Login(const string& identifier, const string& password);
     static bool Register(const string& username, const string& email, const string& password);
     static string generateNextUID();
     static bool checkEmailExists(const string& email);
     static bool checkUsernameExists(const string& username);
 
-    // Instance methods
+   
     void createUserFiles();
     void setGlobalPaths();
     
-    // Getters
+ 
     string getUID() const { return uid; }
     string getUsername() const { return username; }
     string getEmail() const { return email; }
